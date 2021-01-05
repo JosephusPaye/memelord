13a14a77bbd50780d6ace4923bcf90f75cb4f66b
Josephus Paye II <j.paye96@gmail.com>
2021-01-05 21:10:19 +1100

    Allow for restricting access to `/award` using an env variable

--- src/commands/award.ts
+++ src/commands/award.ts
@@ -4,12 +4,41 @@ import { SlackBotkitWorker, debug } from '../bot';
 import { AppStorage } from '../storage';
 import { BotError, BotErrorType } from '../feedback';
 
+let restrictAwardTo: { [key: string]: string[] } | undefined = undefined;
+
+try {
+    restrictAwardTo = JSON.parse(process.env.RESTRICT_AWARD_TO ?? '{}');
+    if (restrictAwardTo) {
+        Object.entries(restrictAwardTo).forEach(([teamId, value]) => {
+            if (!Array.isArray(value)) {
+                throw new Error(
+                    `RESTRICT_AWARD_TO.${value} is not an array (should be an array of Slack user ID strings).`
+                );
+            }
+        });
+    }
+} catch (err) {
+    console.error('invalid RESTRICT_AWARD_TO env variable', err);
+}
+
 export async function award(
     bot: SlackBotkitWorker,
     message: BotkitMessage,
     appStorage: AppStorage
 ) {
     const teamId = message.incoming_message.channelData.team_id;
+
+    // Allow only users listed in the RESTRICT_AWARD_TO env variable if it's set
+    // and available for this team
+    if (restrictAwardTo && restrictAwardTo[teamId] !== undefined) {
+        if (restrictAwardTo[teamId].includes(message.user) === false) {
+            await bot.reply(
+                message,
+                `I'm sorry <@${message.user}>, but I'm afraid I can't let you do that.`
+            );
+            return;
+        }
+    }
 
     const awardees = extractAwardees(message.text);
 
