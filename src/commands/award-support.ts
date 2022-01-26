0c66cddd00d6e3fe091e5090b6f2a95e089a7291
Josephus Paye II <j.paye96@gmail.com>
2022-01-26 14:15:52 +1100

    Don't award a user in more than one place

--- src/commands/award-support.ts
+++ src/commands/award-support.ts
@@ -63,10 +63,28 @@ async function extractAwardeesFromTally(
 ): Promise<string[][]> {
     const candidates = await tallyMessages(bot, message, appStorage);
 
-    const groups = groupBy(candidates, 'reactionCount')
+    const usersAlreadyAwarded = new Set();
+
+    const groups = groupBy(candidates, 'reactionCount') // Group list of candidates by reaction count
+        // Sort the groups by reaction count, high to low
         .sort((groupA, groupB) => {
             return groupB[0].reactionCount - groupA[0].reactionCount;
         })
+        .map((group) => {
+            // Filter the list of users in each group to remove any users that are already in another higher ranking
+            // group. This will ensure each user is only awarded once, for their post with the most reactions.
+            return group.filter((candidate) => {
+                if (usersAlreadyAwarded.has(candidate.user)) {
+                    return false;
+                } else {
+                    usersAlreadyAwarded.add(candidate.user);
+                    return true;
+                }
+            });
+        })
+        // Remove any groups that are now empty as a result of the filter above
+        .filter((group) => group.length > 0)
+        // Take the first 3 groups for first, second, and third place
         .slice(0, 3);
 
     const places = groups.map((group) => {
